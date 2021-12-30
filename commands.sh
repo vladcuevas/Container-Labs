ba2ca18b7a39340b2de6f3641bbd0e4f97612246
vladcuevas <vladcuevas@gmail.com>
2021-12-30 10:33:50 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-# List of common commands, self explanatory
+# Common commands, self explanatory
 
 docker version
 docker info
@@ -15,3 +15,16 @@ docker container run --publish 80:80 --detach --name webhost nginx
 docker container logs webhost
 docker container --help
 docker container rm -f de6 e3c dab
+
+#MongoDB
+docker run --name mongo -d mongo
+
+#MYSQL
+docker rm docker pull --platform linux/amd64 mysql
+docker container run --publish 3306:3306 --name dbmysql -e MYSQL_RANDOM_ROOT_PASSWORD=yes -d mysql    
+
+#nginx
+docker container run --publish 80:80 --detach --name webhost nginx
+
+#HTTPD
+docker container run --publish 8080:80 --detach --name httpd_name httpd
