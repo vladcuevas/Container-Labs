b6025323d338fffacf46519432b7404b770421c7
vladcuevas <vladcuevas@gmail.com>
2021-12-30 14:13:44 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,19 +1,11 @@
 # Common commands, self explanatory
-
+docker container --help
 docker version
 docker info
-docker container run
-docker run
-docker container run --publish 80:80 ngnix
-docker container run --publish 80:80 nginx
-docker container run --publish 80:80 --detach nginx
 docker container ls
-docker container stop e3c897ca4543
-docker ls
 docker container ls -a
 docker container run --publish 80:80 --detach --name webhost nginx
-docker container logs webhost
-docker container --help
+docker container stop e3c897ca4543
 docker container rm -f de6 e3c dab
 
 #MongoDB
@@ -28,3 +20,35 @@ docker container run --publish 80:80 --detach --name webhost nginx
 
 #HTTPD
 docker container run --publish 8080:80 --detach --name httpd_name httpd
+
+#List Docker Images
+docker image ls
+
+#CLI process monitoring
+
+#Performance stats of all containers
+docker container stats
+docker container stats --help
+
+#Detail one container config
+docker container inspect
+
+#Process list in one container
+docker containser top
+
+#Getting a shell insider containers
+
+#Start new container interactively
+docker container run -it
+docker container run -it --name ubuntu ubuntu
+## Re-run a container interactively
+docker container start -ai ubuntu
+# Example
+docker pull alpine
+docker image ls
+docker container run -it alpine bash
+docker container run -it alpine sh
+
+#Run additional command in existing container
+docker container exec -it
+docker container exec -it the-container-name bash
